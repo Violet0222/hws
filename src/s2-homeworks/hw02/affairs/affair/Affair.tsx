bd7585b22118474d9b4f17b7ea2fa7a875276278
Violet <kolosova.dev@gmail.com>
2022-12-24 10:57:56 +0100

    add props priority

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -27,7 +27,7 @@ function Affair(props: AffairPropsType) {
         {/**/}
       </div>
       <div id={"hw2-priority-" + props.affair._id} hidden>
-        {/**/}
+        {props.affair.priority}
       </div>
 
       <button
