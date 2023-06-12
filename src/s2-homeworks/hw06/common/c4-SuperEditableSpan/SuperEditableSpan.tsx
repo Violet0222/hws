412ace09cd08a1123a9de4b8ec4c497815ae0e80
Violet <kolosova.dev@gmail.com>
2023-06-12 12:07:11 +0200

    change initial status of editMode to false HW6

--- src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -38,7 +38,7 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = ({
 
   ...restProps // все остальные пропсы попадут в объект restProps
 }) => {
-  const [editMode, setEditMode] = useState<boolean>(true);
+  const [editMode, setEditMode] = useState<boolean>(false);
   const { children, onDoubleClick, className, defaultText, ...restSpanProps } =
     spanProps || {};
 
