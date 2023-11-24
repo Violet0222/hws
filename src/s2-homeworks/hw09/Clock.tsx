5e454585a5995bf0e6f84b5c849c767c311a4cee
Violet <kolosova.dev@gmail.com>
2023-11-24 21:39:34 +0100

    update HW9

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -67,7 +67,7 @@ function Clock() {
         onMouseEnter={onMouseEnter}
         onMouseLeave={onMouseLeave}
       >
-        <span id={"hw9-day"}>{stringDay},</span>
+        <span id={"hw9-day"}>{stringDay}</span>,
         <span id={"hw9-time"}>
           <strong>{stringTime}</strong>
         </span>
